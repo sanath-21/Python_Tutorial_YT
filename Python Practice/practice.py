a9e0b4f1131b95bbc20453a45a8bf01e102ba4c0
sanath-21 <sanathnayak733@gmail.com>
2024-07-19 20:07:10 +0530

    getting minimum of two numbers

--- Python Practice/practice.py
+++ Python Practice/practice.py
@@ -23,3 +23,18 @@
 #         print(f"{num2} is greater than {num1}")
 
 # maximum()
+
+# 3) Write a Python code to find the Minimum of these two numbers.
+
+# def minimum():
+#     num1 = int(input("Enter 1st Number: "))
+#     num2 = int(input("Enter 2nd Number: "))
+
+#     if num1>num2:
+#         print(f"{num2} is less than {num1}")
+#     elif num1==num2:  
+#         print(f"{num1} is equal to {num2}") 
+#     else:
+#         print(f"{num1} is less than {num2}")
+
+# minimum()
