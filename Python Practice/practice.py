1fa7a60f922065d20b94c6a06ab7d5942fae1474
sanath-21 <sanathnayak733@gmail.com>
2024-07-19 20:02:24 +0530

    practicing python problem

--- Python Practice/practice.py
+++ Python Practice/practice.py
@@ -9,7 +9,17 @@
     
 # User()
 
-# 2) Python Program to Swap Two Elements in a List
-l = [1,2,3,4]
+# 2) Write a Python code to find the Maximum of these two numbers.
 
-print(l)
+# def maximum():
+#     num1 = int(input("Enter 1st Number: "))
+#     num2 = int(input("Enter 2nd Number: "))
+
+#     if num1>num2:
+#         print(f"{num1} is greater than {num2}")
+#     elif num1==num2:  
+#         print(f"{num1} is equal to {num2}") 
+#     else:
+#         print(f"{num2} is greater than {num1}")
+
+# maximum()
