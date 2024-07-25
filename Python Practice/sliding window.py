794e573780f0b418ca3af9d33b85fd47b97a0def
sanath-21 <sanathnayak733@gmail.com>
2024-07-25 20:14:09 +0530

    adding comments

--- Python Practice/sliding window.py
+++ Python Practice/sliding window.py
@@ -1,10 +1,10 @@
 # paragraph =  ["Hello","Wor5d", "Good", "Job","Hii","Be Careful"]
-paragraph = [input(f"Enter the {i+1} value: ") for i in range(5)]
+paragraph = [input(f"Enter the {i+1} value: ") for i in range(5)] #get input values from user
 
 
-for i in paragraph:
-    for j in range (0, len(i)):
+for i in paragraph:   #store each word in i
+    for j in range (0, len(i)): #will iterate from 0 to len of each word
     
-        if((j+1)<len(i)):
+        if((j+1)<len(i)): 
             if(i[j]==i[j+1]):
                 print(i)
